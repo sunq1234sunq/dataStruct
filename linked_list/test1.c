2f1a69f10c2dd1f2d09ee9f051bd4f5ec9b6be93
lyj <995325198@qq.com>
2026-01-06 01:23:45 +0800

    尾插法，循环遍历

--- linked_list/test1.c
+++ linked_list/test1.c
@@ -13,12 +13,55 @@ Node* initList(){
     head -> data = 0;
     head -> next = NULL;
     return head;
-}Node;
-
-int main(){
-    Node *list = initList();
-    return 1;
 }
 
+Node* getTail(Node *L){
+    if(L == NULL) return NULL;
 
+    Node *current = L;
+    while(current -> next != NULL){
+        current = current -> next;
+    }
+    return current;
+}
+
+void printList(Node *L){
+    Node *current = L -> next;
+    while(current != NULL){
+        printf("%d ->", current -> data);
+        current = current -> next;
+    }
+    printf(" NULL \n");
+}
+
+void insertAtHead(Node *L, Elemtype e){
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode -> data = e;
+    newNode -> next = L -> next;
+    L -> next = newNode;
+}
+
+void insertAtTail(Node *Tail, Elemtype e){
+    if (Tail == NULL) return;
     
+    Node *newNode = (Node*)malloc(sizeof(Node));
+
+    newNode -> data = e;
+    newNode -> next = NULL;
+    Tail -> next = newNode;
+
+}
+
+int main(){
+    Node *list = initList(); // 返回的是头节点的指针
+
+    insertAtHead(list, 10);
+    insertAtHead(list, 20);
+    insertAtHead(list, 30);
+
+    Node *Tail = getTail(list);
+    insertAtTail(Tail, 40);
+    printList(list);
+
+    return 0;
+}
